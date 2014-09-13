5b22d0be242a0085e8cee570aed02cf744112cb2
Moyamo <moyamodehacker@gmail.com>
2014-09-13 20:23:15 +0200

    Add clear button to delete polygon from canvas
    Added a clear button. When the clear button is pressed, the canvas is
    cleared and the points in 'points' deleted.

--- willys-remodeller.py
+++ willys-remodeller.py
@@ -14,20 +14,34 @@ def add_point(event):
     """Adds a point to the 'points' list and draw it on the canvas"""
     global last_line
 
+    # Create the point
     points.append((event.x, event.y))
-    canvas.create_oval((event.x - 2, event.y - 2, event.x + 2, event.y + 2))
+    i = canvas.create_oval((event.x - 2, event.y - 2, event.x + 2, event.y + 2))
+    canvas.addtag('point', 'withtag', i)
 
+    # Connect lines to the point
     if len(points) > 1:
         point1 = points[-1]
         point2 = points[-2]
-        canvas.create_line((point1[0], point1[1], point2[0], point2[1]))
+        i = canvas.create_line((point1[0], point1[1], point2[0], point2[1]))
+        canvas.addtag('line', 'withtag',  i)
     if len(points) >= 3:
         point1 = points[-1]
         point2 = points[0]
         if last_line != None:
             canvas.delete(last_line)
         last_line = canvas.create_line((point1[0], point1[1], point2[0], point2[1]))
+        canvas.addtag('line', 'withtag', last_line)
     print(event.x, event.y)
+
+def clear_canvas():
+    """Deletes the current polygon from the canvas."""
+    global points
+    # Delete all points
+    points = list()
+    # Clear lines and points from the canvas
+    canvas.delete('line', 'point')
+
 
 def squarify_polygon():
     pass
@@ -37,8 +51,10 @@ frame = ttk.Frame(root)
 canvas = Canvas(frame, width=canvas_height*PHI, height=canvas_height)
 canvas.bind('<Button-1>', add_point)
 squarify = ttk.Button(frame, text='Squarify polygon', command = squarify_polygon)
+clear = ttk.Button(frame, text='Clear', command = clear_canvas)
 frame.grid()
 canvas.grid()
 squarify.grid()
+clear.grid()
 root.mainloop()
 

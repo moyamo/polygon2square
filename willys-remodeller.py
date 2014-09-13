2812c79ef3db79e1844fc7817766a9c67ab1a0ab
Moyamo <moyamodehacker@gmail.com>
2014-09-13 20:13:46 +0200

    Click to draw polygon on canvas
    When the user clicks on the canvas it will add a point to the polygon
    currently being constructed.

--- willys-remodeller.py
+++ willys-remodeller.py
@@ -6,12 +6,36 @@ from tkinter import ttk
 PHI = (1 + 5**0.5) / 2
 canvas_height = 300
 
+# A list of points the user placed on the canvas
+points = list()
+last_line = None
+
+def add_point(event):
+    """Adds a point to the 'points' list and draw it on the canvas"""
+    global last_line
+
+    points.append((event.x, event.y))
+    canvas.create_oval((event.x - 2, event.y - 2, event.x + 2, event.y + 2))
+
+    if len(points) > 1:
+        point1 = points[-1]
+        point2 = points[-2]
+        canvas.create_line((point1[0], point1[1], point2[0], point2[1]))
+    if len(points) >= 3:
+        point1 = points[-1]
+        point2 = points[0]
+        if last_line != None:
+            canvas.delete(last_line)
+        last_line = canvas.create_line((point1[0], point1[1], point2[0], point2[1]))
+    print(event.x, event.y)
+
 def squarify_polygon():
     pass
 
 root = Tk()
 frame = ttk.Frame(root)
 canvas = Canvas(frame, width=canvas_height*PHI, height=canvas_height)
+canvas.bind('<Button-1>', add_point)
 squarify = ttk.Button(frame, text='Squarify polygon', command = squarify_polygon)
 frame.grid()
 canvas.grid()

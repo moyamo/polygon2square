99282217541dbbda1dd633db57fcb8156bb57648
moyamo <moyamodehacker@gmail.com>
2014-11-09 22:14:48 +0200

    Add method to_rightangle to Triangle
    Triangle can be split into two right-angled triangles by calling
    to_rightangle.

--- geometry.py
+++ geometry.py
@@ -51,6 +51,11 @@ class LineSegment:
         """
         self.points = (point1, point2)
 
+    def length(self):
+        x1, y1 = self.points[0]
+        x2, y2 = self.points[1]
+        return math.hypot(x1 - x2, y1 - y2)
+
     def to_line(self):
         """Converts this LineSegment into a Line (by extending both ends)
 
@@ -81,9 +86,9 @@ class Line:
         worry about homogenizing the coordinates.
         """
         sums = A + B + C
-        self.A = A
-        self.B = B
-        self.C = C
+        self.A = A / sums
+        self.B = B / sums
+        self.C = C / sums
 
     def side_of_line(self, point):
         """Returns the number 1, 0, -1 if point is on the positive side, on the
@@ -115,6 +120,14 @@ class Line:
         y = (A2*C1 - A1*C2) / (A1 * B2 - A2 * B1)
         x = (B1*C2 - B2*C1) / (A1 * B2 - A2 * B1)
         return (x, y)
+
+    def perpendicular(self, point):
+        """Return a line perpendicular to this one passing through point"""
+        x, y = point
+        A = -self.B
+        B = self.A
+        C = -A*x - B*y
+        return Line(A, B, C)
     
     def __repr__(self):
         return '(' + ', '.join(str(s) for s in [self.A, self.B, self.C]) + ')'
@@ -137,6 +150,36 @@ class Triangle:
     def __init__(self, tpl):
         """tpl is a 3-tuple of coordinates"""
         self.points = tpl
+
+    @property
+    def segments(self):
+        """A list of segments representing the sides of the line.
+        
+        The ith line will be opposite the ith point
+        """
+        return [LineSegment(self.points[1], self.points[2]),
+                LineSegment(self.points[0], self.points[2]),
+                LineSegment(self.points[0], self.points[1])
+                ]
+
+    def angle(self, i):
+        """Return the angle at the ith point"""
+        segs = self.segments
+        a = segs[i].length()
+        b = segs[(i + 1) % 3].length()
+        c = segs[(i + 2) % 3].length()
+        return math.acos((a**2 - b**2 - c**2)/(-2*b*c))
+    
+    def largest_angle(self):
+        """Return the the number of the point at the largest angle"""
+        cur_max = 0
+        big_ang = None
+        for i in range(len(self.points)):
+            ang = self.angle(i)
+            if ang > cur_max:
+                cur_max = ang
+                big_ang = i
+        return big_ang
     
     def rotate(self, pivot, rangle):
         """Return a new triangle rotate clockwise (by angle) around pivot.
@@ -161,13 +204,19 @@ class Triangle:
         new_points = [(x + tx, y + ty) for x, y in self.points]
         return Triangle(tuple(new_points))
 
-    @property
-    def segments(self):
-        """A list of segments representing the sides of the line."""
-        return [LineSegment(self.points[0], self.points[1]),
-                LineSegment(self.points[0], self.points[2]),
-                LineSegment(self.points[1], self.points[2])
-                ]
+    def to_rightangle(self):
+        """Splits the triangle into two right-angled triangles"""
+        # We need to cut the triangle across the largest angle (in case it's
+        # obtuse)
+        p = self.points
+        big_point = self.largest_angle()
+        other_points = [(big_point + 1) % 3, (big_point + 2) % 3]
+        cut = self.segments[big_point].to_line().perpendicular(p[big_point])
+        new_point = line_intersects_segment(cut, self.segments[big_point])
+        t1 =  Triangle((p[big_point], new_point, p[other_points[0]]))
+        t2 =  Triangle((p[big_point], new_point, p[other_points[1]]))
+        return (t1, t2)
+        
 
 
     def split(self, line):
@@ -218,12 +267,12 @@ class Triangle:
             if len(sided_points[0]) == 1:
                 t1 = Triangle((sided_points[0][0], intersects[0], intersects[1]))
                 t2 = Triangle((sided_points[1][0], intersects[0], intersects[1]))
-                t3 = Triangle((sided_points[1][0], sided_points[1][1], intersects[1]))
+                t3 = Triangle((sided_points[1][0], sided_points[1][1], intersects[0]))
                 return (Shape([t1]), Shape([t2, t3]))
             elif len(sided_points[1]) == 1:
                 t1 = Triangle((sided_points[1][0], intersects[0], intersects[1]))
                 t2 = Triangle((sided_points[0][0], intersects[0], intersects[1]))
-                t3 = Triangle((sided_points[0][0], sided_points[0][1], intersects[1]))
+                t3 = Triangle((sided_points[0][0], sided_points[0][1], intersects[0]))
                 return (Shape([t2, t3]), Shape([t1]))
             else:
                 raise Exception("Segments missing")

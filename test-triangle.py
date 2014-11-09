c76b4a271ceca84d32896898e16dbf5a0d8c6f8c
moyamo <moyamodehacker@gmail.com>
2014-11-09 20:59:47 +0200

    Add button to split triangle in testing
    The splitting of Triangle can now be tested with test-triangle.

--- test-triangle.py
+++ test-triangle.py
@@ -2,29 +2,69 @@
 
 from tkinter import *
 from tkinter import ttk
-from geometry import Triangle
+from geometry import *
+from random import randint
+from collections import defaultdict
 import math
 
 PHI = (1 + 5**0.5) / 2
 canvas_height = 300
+TRIANGLE_MODE = 0
+LINE_MODE = 1
+mode = TRIANGLE_MODE
 
 # A temporary list of points the user placed on the canvas
 points = list()
 # A dictionary of triangles
 triangles = dict()
+def random_color():
+    color = randint(0, 0xFFFFFF)
+    return hex_color(color)
+
+# A dictionary that colors the triangle
+colors = defaultdict(random_color)
 # pivot to rotate around
 global_pivot = None
+
+def hex_color(color):
+    return '#' + hex(color)[2:].rjust(6, '0')
+
+def draw_shape(shape):
+    global color, color_inc
+    tris = shape.triangles
+    for t in tris:
+        i = canvas.create_polygon(t.points, fill=colors[t])
+        canvas.addtag('triangle', 'withtag', i)
+        canvas.tag_bind(i, '<Button-1>', rotate_triangle(i))
+        triangles[i] = t
+
 
 def add_point(event):
     """Adds a point to the canvas. If there are three loose points, they will
     be connected to form a triangle."""
+    global mode, triangles
     ex, ey = event.x, event.y
     points.append((ex, ey))
     i = canvas.create_oval((ex - 2, ey - 2, ex + 2, ey + 2))
     canvas.addtag('point', 'withtag', i)
-    while len(points) >= 3:
+    if mode == LINE_MODE and len(points) >= 2:
+        p1, p2 = points.pop(), points.pop()
+        x1, y1 = p1
+        x2, y2 = p2
+        i = canvas.create_line((x1, y1, x2, y2))
+        canvas.addtag('line', 'withtag', i)
+        new_shapes = list()
+        for t in triangles.values():
+            u, d = t.split(LineSegment(p1, p2).to_line())
+            new_shapes.append(u)
+            new_shapes.append(d)
+        canvas.delete('triangle')
+        triangles = dict()
+        for s in new_shapes:
+            draw_shape(s)
+        mode = TRIANGLE_MODE
+    while len(points) >= 3 and mode == TRIANGLE_MODE:
         p1, p2, p3 = points.pop(), points.pop(), points.pop()
-        triangles
         i = canvas.create_polygon((p1, p2, p3))
         canvas.addtag('triangle', 'withtag', i)
         canvas.tag_bind(i, '<Button-1>', rotate_triangle(i))
@@ -43,7 +83,7 @@ def rotate_triangle(i):
         print(rotate.get() * math.pi / 180)
         del triangles[i]
         canvas.delete(i)
-        ni = canvas.create_polygon(new_tri.points)
+        ni = canvas.create_polygon(new_tri.points, fill=colors[new_tri])
         canvas.addtag('triangle', 'withtag', ni)
         canvas.tag_bind(ni, '<Button-1>', rotate_triangle(ni))
         triangles[ni] = new_tri
@@ -54,7 +94,11 @@ def clear_canvas():
     points = list()
     triangles = dict()
     global_pivot = None
-    canvas.delete('triangle', 'point')
+    canvas.delete('triangle', 'point', 'line')
+
+def set_state_line():
+    global mode
+    mode = LINE_MODE
 
 root = Tk()
 frame = ttk.Frame(root)
@@ -67,10 +111,12 @@ rotate_label = ttk.Label(frame, text=rotate.get())
 rotate_display =  StringVar()
 rotate_label['textvariable'] = rotate_display
 clear = ttk.Button(frame, text='Clear', command = clear_canvas)
+cutLine = ttk.Button(frame, text='Cut By Line', command = set_state_line)
+
 frame.grid()
 canvas.grid()
 rotate.grid()
 rotate_label.grid()
 clear.grid()
+cutLine.grid()
 root.mainloop()
-
